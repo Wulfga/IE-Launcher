c4c4413945b48dedbc2b09cf617f688384074d85
Wulfga <>
2023-04-20 11:54:50 +0200

    First commit

---
+++ IE Launcher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IE_Launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var IE = new SHDocVw.InternetExplorer();
+            object URL = "www.google.de";
+
+            IE.ToolBar = 0;
+            IE.StatusBar = false;
+            IE.MenuBar = false;
+
+            //
+            //IE.Width = 999;
+            //IE.Height = 999;
+            IE.Visible = true;
+
+            //Open URL
+            IE.Navigate2(ref URL);
+        }
+    }
+}
